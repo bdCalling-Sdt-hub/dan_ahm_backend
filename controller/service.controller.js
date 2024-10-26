d259dc8d32359897896980a060caf64d2e549b43
abir <antorabir@gmail.com>
2024-10-26 10:03:10 +0600

    bug fix: negative values handled

--- controller/service.controller.js
+++ controller/service.controller.js
@@ -89,8 +89,12 @@ const updateServiceById = async (req, res) => {
 
 const getAllServices = async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+    if (page < 1) page = 1;
+    if (limit < 1) limit = 10;
+
     const skip = (page - 1) * limit;
 
     const services = await Service.find({ isDeleted: false })
