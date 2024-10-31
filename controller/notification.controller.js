02ccebac75de79e12f00a268a111880d8e752c4d
abir <antorabir@gmail.com>
2024-10-31 10:34:36 +0600

    add: notification
    route & controller added for getting all notifications

--- controller/notification.controller.js
+++ controller/notification.controller.js
@@ -1,4 +1,5 @@
 const User = require("../model/user.model");
+const Notification = require("../model/notification.model");
 const HTTP_STATUS = require("../constants/statusCodes");
 
 // Controller to get notifications by userId
@@ -35,6 +36,28 @@ const getNotificationsByUserId = async (req, res) => {
   }
 };
 
+const getAllNotifications = async (req, res) => {
+  try {
+    // Fetch the user to check if they exist
+    const notifications = await Notification.find();
+
+    if (!notifications) {
+      return res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .send(failure("User does not exist"));
+    }
+
+    res.status(HTTP_STATUS.OK).send({
+      message: "Notifications fetched successfully",
+      notifications: notifications,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 module.exports = {
   getNotificationsByUserId,
+  getAllNotifications,
 };
--- routes/notification.route.js
+++ routes/notification.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const routes = express();
 const {
   getNotificationsByUserId,
+  getAllNotifications,
 } = require("../controller/notification.controller");
 
 // Route to get notifications by userId
-routes.get("/user/notifications/:userId", getNotificationsByUserId);
-
+routes.get("/get-notifications-by-user/:userId", getNotificationsByUserId);
+routes.get("/get-all-notifications", getAllNotifications);
 module.exports = routes;
