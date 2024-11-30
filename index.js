cb64f05df5d9d9989da4ef52070d89a9e7e9467b
abir <antorabir@gmail.com>
2024-11-30 14:48:02 +0600

    add: help module

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const prescriptionRouter = require("./routes/prescription.route");
 const tipRouter = require("./routes/tip.route");
 const notificationRouter = require("./routes/notification.route");
 const helpRouter = require("./routes/help.route");
+const aboutUsRouter = require("./routes/aboutUs.route");
 
 const app = express();
 
@@ -65,6 +66,7 @@ app.use("/faq", faqRouter);
 app.use("/tip", tipRouter);
 app.use("/notification", notificationRouter);
 app.use("/help", helpRouter);
+app.use("/about-us", aboutUsRouter);
 
 // Route to handle all other invalid requests
 app.use((req, res) => {
